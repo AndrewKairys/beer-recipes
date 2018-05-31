64235147e51065d73060639d4a4bfaf6ade48d91
NolanHughes <Nhughes987@gmail.com>
2018-05-31 11:00:00 -0500

    Finish refactoring add-hop-button and add-fermentable-button js functionality using handlebars. Add handlebars_ingredients partial. Move update fermentable and hop indexes functions to recipes.js.

--- app/assets/javascripts/recipes-new-edit.js
+++ app/assets/javascripts/recipes-new-edit.js
@@ -1,4 +1,5 @@
 $(".recipes.new, .recipes.edit").ready(function() {
+  //CUSTOM SORT FUNCTION
   const sort_by = function() {
     const fields = [].slice.call(arguments)
 
@@ -19,37 +20,41 @@ $(".recipes.new, .recipes.edit").ready(function() {
     }
   }
 
-  $(function() {
-    $("#add-fermentable-button").click(function() {
-      let fermentableCounter = parseInt($(".recipe_fermentable_ids").last().attr("data_value")) + 1
+  //HANDLEBARS HELPERS
+  Handlebars.registerHelper('fermentableCounter', function() {
+    return parseInt($(".recipe_fermentable_ids").last().attr("data_value")) + 1
+  })
 
-      $("#fermentables-selection").append(`<div class="fermentables-dropdown"><select name="recipe[fermentable_ids][]" class="col-md-6 recipe_fermentable_ids" data_value="${fermentableCounter}" onchange="updateFermentableIndexes(this.value, ${fermentableCounter}, '')"><option value=""></option><% fermentables.sort_by { |f| f.name.downcase }.each do |fermentable| %><option value='<%=fermentable.id%>'><%= fermentable.name %></option><% end %></select></div><div class="col-md-6"><div class="col-md-6 input-label"><label>Amount:</label></div><div class="col-md-6 fermentable-amount-input-div-${fermentableCounter}"><input type="number" name="" value="" class="fermentable-amount-input"> (lbs)</div></div><br>`)
-    })
+  Handlebars.registerHelper('hopCounter', function() {
+    return parseInt($(".recipe_hop_ids").last().attr("data_value")) + 1
+  })
 
-    $("#add-hop-button").click(function() {
-      let hopCounter = parseInt($(".recipe_hop_ids").last().attr("data_value")) + 1
+  //ONCLICK FUNCTIONS
+  $("#add-fermentable-button").click(function() {
+    fetch("/fermentables.json", { credentials: 'include' })
+    .then(res => res.json())
+    .then(fermentables => {
+      const templateScript = $("#fermentable-dropdown-template").html();
+      const template = Handlebars.compile(templateScript);
+      sortFermentables(fermentables)
+      const result = template(fermentables);
 
-      $("#hops-selection").append(`<div class="hops-dropdown"><select name="recipe[hop_ids][]" class="col-md-4 recipe_hop_ids" data_value="${hopCounter}" onchange="updateHopIndexes(this.value, ${hopCounter}, '', '')"><option value=""></option><% hops.sort_by { |h| h.name.downcase }.each do |hop| %><option value='<%=hop.id%>'><%= hop.name %></option><% end %></select></div><div class="col-md-4"><div class="col-md-6 input-label"><label>Amount:</label></div><div class="col-md-6 hop-amount-input-div-${hopCounter}"><input type="number" name="" value="" class="hop-amount-input"> (oz)</div></div><div class="col-md-4"><div class="col-md-6 input-label"><label>Addition Time:</label></div><div class="col-md-6 hop-addition-time-input-div-${hopCounter}"><input type="number" name="" value="" class="hop-amount-input"> (min)<br></div></div>`)
+      $("#fermentable-selection").append(result)
     })
   })
 
-  function updateFermentableIndexes(id, index, amount) {
-    if(id === "") {
-      $(`.fermentable-amount-input-div-${index}`).html(`<input type="number" name="" value="" class="fermentable-amount-input"> (lbs)`)
-    } else {
-      $(`.fermentable-amount-input-div-${index}`).html(`<input type="number" name="recipe[recipe_fermentable_attributes][${id}][amount]" value="${amount}" class="fermentable-amount-input"> (lbs)`)
-    }
-  }
+  $("#add-hop-button").click(function() {
+    fetch("/hops.json", { credentials: 'include' })
+    .then(res => res.json())
+    .then(hops => {
+      const templateScript = $("#hop-dropdown-template").html();
+      const template = Handlebars.compile(templateScript);
+      sortHops(hops)
+      const result = template(hops);
 
-  function updateHopIndexes(id, index, amount, addition_time) {
-    if(id === "") {
-      $(`.hop-amount-input-div-${index}`).html(`<input type="number" name="" value="" class="hop-amount-input"> (oz)`)
-      $(`.hop-addition-time-input-div-${index}`).html(`<input type="number" name="" value="" class="hop-amount-input"> (min)<br>`)
-    } else {
-      $(`.hop-amount-input-div-${index}`).html(`<input type="number" name="recipe[recipe_hop_attributes][${id}][amount]" value="${amount}" class="hop-amount-input"> (oz)`)
-      $(`.hop-addition-time-input-div-${index}`).html(`<input type="number" name="recipe[recipe_hop_attributes][${id}][addition_time]" value="${addition_time}" class="hop-amount-input"> (min)<br>`)
-    }
-  }
+      $("#hop-selection").append(result)
+    })
+  })
 
   $("#add-yeast-button").click(function() {
     fetch("/yeasts.json", { credentials: 'include' })
@@ -63,6 +68,16 @@ $(".recipes.new, .recipes.edit").ready(function() {
       $("#yeast-selection").append(result)
     })
   })
+
+  //SORT FUNCTIONS FOR EACH INGREDIENT CATEGORY
+  //Could be more DRY by using sortIngredients once for hops and fermentables but that make it confusing since yeasts are sorted differently
+  function sortFermentables(fermentables) {
+    fermentables.sort(sort_by('name'))
+  }
+
+  function sortHops(hops) {
+    hops.sort(sort_by('name'))
+  }
 
   function sortYeasts(yeasts) {
     yeasts.sort(sort_by('brand', 'variety'))
--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,1 +1,17 @@
+function updateFermentableIndexes(id, index, amount) {
+  if(id === "") {
+    $(`.fermentable-amount-input-div-${index}`).html(`<input type="number" name="" value="" class="fermentable-amount-input"> (lbs)`)
+  } else {
+    $(`.fermentable-amount-input-div-${index}`).html(`<input type="number" name="recipe[recipe_fermentable_attributes][${id}][amount]" value="${amount}" class="fermentable-amount-input"> (lbs)`)
+  }
+}
 
+function updateHopIndexes(id, index, amount, addition_time) {
+  if(id === "") {
+    $(`.hop-amount-input-div-${index}`).html(`<input type="number" name="" value="" class="hop-amount-input"> (oz)`)
+    $(`.hop-addition-time-input-div-${index}`).html(`<input type="number" name="" value="" class="hop-amount-input"> (min)<br>`)
+  } else {
+    $(`.hop-amount-input-div-${index}`).html(`<input type="number" name="recipe[recipe_hop_attributes][${id}][amount]" value="${amount}" class="hop-amount-input"> (oz)`)
+    $(`.hop-addition-time-input-div-${index}`).html(`<input type="number" name="recipe[recipe_hop_attributes][${id}][addition_time]" value="${addition_time}" class="hop-amount-input"> (min)<br>`)
+  }
+}
