87e53ebbc5a8cfd6ae75b922c654a42dd6c864f2
NolanHughes <Nhughes987@gmail.com>
2018-06-06 11:55:31 -0500

    Move new comment js to recipes-show.js.

--- app/assets/javascripts/recipes-show.js
+++ app/assets/javascripts/recipes-show.js
@@ -120,4 +120,40 @@ function loadRecipe() {
   })
 }
 
+$(function() {
+  $("#new_comment").on("submit", function(e) {
+
+    $.ajax({
+      type: ($("input[name='_method']").val() || this.method),
+      url: this.action,
+      data: $(this).serialize()
+    }).success(function() {
+      const userId = parseInt($(".js-next").attr("data-user-id"))
+      const recipeId = parseInt($(".js-next").attr("data-id"));
+
+      $.get(`/users/${userId}/recipes/${recipeId}.json`, function(data) {
+        if($(".comment-body-input").val() !== "") {
+          function Comment(body, user) {
+            this.body = body
+            this.user = user
+          }
+
+          const comment = new Comment(data.comments.slice(-1)[0].body, data.comments.slice(-1)[0].user.name)
+
+          Comment.prototype.post = function() {
+            return this.body + " - Posted by: " + this.user;
+          };
+
+          $(".comment-body-input").val("")
+          $("#comments").append(`<li>${comment.post()}</li>`)
+        } else {
+          alert("Please type a comment with some content.")
+        }
+      })
+    })
+
+    e.preventDefault()
+  })
+})
+
 loadRecipe()
